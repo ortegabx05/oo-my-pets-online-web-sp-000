43bee921f64e0b8a325e6e3f11876b3bd29a50ff
jessi ortega <ortegabx05@gmail.com>
2019-08-05 02:18:16 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+  
+  @@all = []
+  
+  def initialize(name,mood)
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
-  # code goes here
+  attr_reader :species, :name
+  attr_accessor :cats, :dogs
+
+
+ @@all = []
+
+  def initialize(name)
+    @name = name
+    @cats = []
+    @dogs = []
+    @species = species
+    @@all << self
+  end
+  
+  
+  def species
+    @species = "human"
+  end
+  
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    @@all.length
+  end
+  
 end
